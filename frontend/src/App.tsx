aa42eb9910263bba7988dee0f77a0ca2bf477982
Kaue-Medeiros <kaue.medeirosguedes@gmail.com>
2026-05-31 08:27:00 -0300

    Adicionado estilo com CSS e opção de clicar nos cards para obter os detalhes específicos de cada mundo

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,121 +1,92 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
+import { useState, useEffect, Fragment } from 'react'
 import './App.css'
 
+import type { World } from './types'
+
 function App() {
-  const [count, setCount] = useState(0)
+  // <World[]>([]) define que esse Estado é um array de World's, e que o Estado inicial é um array vazio []
+  const [worlds, setWorlds] = useState<World[]>([])
+  const [selecionado, setSelecionado] = useState<World | null>(null)
+
+  const [loading, setLoading] = useState<boolean>(true)
+  const [erro, setErro] = useState<string | null>(null)
+  
+
+
+  // useEffect roda o código após o componente aparecer na tela
+  // Codigo para pegar a informação geral dos mundos, para criar os cards
+  useEffect(() => {
+    async function carregar(){
+      try {
+        const response = await fetch('/api/worlds')
+        if(!response.ok) throw new Error(`HTTP: ${response.status}`) // trata erro do backend
+        const data = await response.json()
+        setWorlds(data.regular_worlds)
+      } catch(error){
+        console.log(`Erro ao carregar os mundos: ${error}`)
+        setErro('Falha ao carregar os mundos!')
+      } finally {
+        setLoading(false) // Sempre 'desliga' a condição de loading
+      }
+    }
+    carregar()
+
+  }, [])
+  // o parametro [] é o "array de dependencias", como está vazio, ele roda apenas uma vez, ao montar
+
+
+  async function selecionarMundo(nome: string) {
+
+    try {
+      const response = await fetch(`/api/world/${nome}`)
+      if(!response.ok) throw new Error(`HTTP: ${response.status}`)
+      const data = await response.json()
+      setSelecionado(data)
+
+    }catch(error){
+      console.log(`Erro ao carregar o mundo: ${error}`)
+    }
+  }
+
+
 
   return (
-    <>
-      <section id="center">
-        <div className="hero">
-          <img src={heroImg} className="base" width="170" height="179" alt="" />
-          <img src={reactLogo} className="framework" alt="React logo" />
-          <img src={viteLogo} className="vite" alt="Vite logo" />
-        </div>
-        <div>
-          <h1>Get started</h1>
-          <p>
-            Edit <code>src/App.tsx</code> and save to test <code>HMR</code>
-          </p>
-        </div>
-        <button
-          type="button"
-          className="counter"
-          onClick={() => setCount((count) => count + 1)}
-        >
-          Count is {count}
-        </button>
-      </section>
+    <Fragment>
+      <p className="mensagem-carregando">{ loading ? 'Carregando' : 'Carregado!'}</p>
+      <p> {erro != null ? erro : worlds.length + ' mundos carregados! '} </p>
 
-      <div className="ticks"></div>
 
-      <section id="next-steps">
-        <div id="docs">
-          <svg className="icon" role="presentation" aria-hidden="true">
-            <use href="/icons.svg#documentation-icon"></use>
-          </svg>
-          <h2>Documentation</h2>
-          <p>Your questions, answered</p>
-          <ul>
-            <li>
-              <a href="https://vite.dev/" target="_blank">
-                <img className="logo" src={viteLogo} alt="" />
-                Explore Vite
-              </a>
-            </li>
-            <li>
-              <a href="https://react.dev/" target="_blank">
-                <img className="button-icon" src={reactLogo} alt="" />
-                Learn more
-              </a>
-            </li>
-          </ul>
-        </div>
-        <div id="social">
-          <svg className="icon" role="presentation" aria-hidden="true">
-            <use href="/icons.svg#social-icon"></use>
-          </svg>
-          <h2>Connect with us</h2>
-          <p>Join the Vite community</p>
-          <ul>
-            <li>
-              <a href="https://github.com/vitejs/vite" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#github-icon"></use>
-                </svg>
-                GitHub
-              </a>
-            </li>
-            <li>
-              <a href="https://chat.vite.dev/" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#discord-icon"></use>
-                </svg>
-                Discord
-              </a>
-            </li>
-            <li>
-              <a href="https://x.com/vite_js" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#x-icon"></use>
-                </svg>
-                X.com
-              </a>
-            </li>
-            <li>
-              <a href="https://bsky.app/profile/vite.dev" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#bluesky-icon"></use>
-                </svg>
-                Bluesky
-              </a>
-            </li>
-          </ul>
-        </div>
-      </section>
 
-      <div className="ticks"></div>
-      <section id="spacer"></section>
-    </>
+      {/* Mapeia cada mundo da lista para criar um card contendo nome, localização, quantidade de players online e status do mundo*/ }
+      {/* Verifica se esta no estado de mostrar a lista de servidores ou os detalhes de um servidor especifico*/}
+
+      <div className="cards">
+        {selecionado ? (
+          <>
+            <div className="world-details">
+              <button className="btn-voltar" onClick={() => setSelecionado(null)}>Voltar</button>
+              <h1>{selecionado.name}</h1>
+              <ul>
+                <li>Status: {selecionado.status}</li>
+                <li>Localização: {selecionado.location}</li>
+                <li>Players Online: {selecionado.players_online}</li>
+                <li>Tipo de Pvp: {selecionado.pvp_type}</li>
+              </ul>
+            </div>
+          </>
+        ) : (
+          worlds.map((world) => (
+            <div className="card" key={world.name} onClick={() => selecionarMundo(world.name)}>
+              <h3>{world.name}</h3>
+              <p>{world.location}</p>
+              <p>{world.players_online}</p>
+              <p>{world.status}</p>
+            </div>
+          ))
+        )}
+      </div>
+      
+    </Fragment>
   )
 }
 
